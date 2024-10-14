f42ca0d8aec7df27b304d3be22e633fe52eb10f2
Maruquitus <marcospauloda.s.s@gmail.com>
2024-10-14 19:24:37 -0300

    feat: questões enem vindas da api

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,8 +1,11 @@
-export default function ActionButton({ text }) {
+export default function ActionButton({ text, className, onClick, disabled }) {
   return (
     <button
-      className='bg-[#FA7807] text-white font-bold p-4 rounded-xl border-2 border-[#FA7807] hover:bg-[#FDAD0B] hover:border-[#FDAD0B] active:bg-transparent active:border-[#FA7807] active:text-[#FA7807]'>
-        {text}
+      disabled={disabled}
+      onClick={() => onClick()}
+      className={`${className} bg-[#FA7807] text-white font-bold p-4 rounded-xl border-2 border-[#FA7807] hover:bg-[#FDAD0B] hover:border-[#FDAD0B] active:bg-transparent active:border-[#FA7807] active:text-[#FA7807] disabled:bg-slate-400 disabled:border-slate-400 disabled:active:text-white disabled:cursor-not-allowed`}
+    >
+      {text}
     </button>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login.jsx";
 import Cadastro from "./pages/Cadastro.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./input.css";
+import { Questões } from "./pages/Questões.jsx";
 
 const router = createBrowserRouter([
   {
@@ -28,6 +29,11 @@ const router = createBrowserRouter([
   {
     path: "/chat",
     element: <Chat />,
+    errorElement: <Erro />,
+  },
+  {
+    path: "/questoes/:prova/:questao",
+    element: <Questões />,
     errorElement: <Erro />,
   },
   {
